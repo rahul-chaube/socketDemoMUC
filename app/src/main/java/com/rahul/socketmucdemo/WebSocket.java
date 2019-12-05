5aaec104e9395fac9683e62bb12901adc3f2d2a4
rahul <chauberahul1993@gmail.com>
2019-12-05 17:16:31 +0530

    Added UI

--- app/src/main/java/com/rahul/socketmucdemo/WebSocket.java
+++ app/src/main/java/com/rahul/socketmucdemo/WebSocket.java
@@ -6,14 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -25,9 +28,12 @@ import java.net.UnknownHostException;
 public class WebSocket extends AppCompatActivity {
     public Socket clientSocket = null;
     String address, port;
-    public static  String IP="192.168.4.1";
-    public static final int PORT=6000;
-    TextView textViewStatus;
+    public static String IP = "192.168.4.1";
+    public static final int PORT = 6000;
+    TextView textViewStatus,textViewShowCommand;
+
+    EditText editTextCommand;
+
     private Socket socket;
 
     @Override
@@ -35,19 +41,29 @@ public class WebSocket extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_socket);
         textViewStatus = findViewById(R.id.socketStatus);
+        editTextCommand=findViewById(R.id.editTextCommand);
+        textViewShowCommand=findViewById(R.id.showCommand);
+
         if (getIntent() != null) {
             address = getIntent().getStringExtra(MainActivity.ADDRESS);
             port = getIntent().getStringExtra(MainActivity.PORT);
-            IP=address.trim();
+//            IP = address.trim();
             Log.e("Host ", address);
 
             Log.e("Port ", " " + port);
-            new UpdateTask().execute();
+//            new UpdateTask().execute();
         }
 
 
-        new Thread(new ClientThread()).start();
 
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Thread(new ClientThread()).start();
     }
 
     private void connect() {
@@ -55,10 +71,10 @@ public class WebSocket extends AppCompatActivity {
         try {
 
             this.clientSocket.connect(new InetSocketAddress(IP, PORT), 5000);
-            textViewStatus.setText("Connected "+clientSocket.isConnected());
+            textViewStatus.setText("Connected " + clientSocket.isConnected()+" "+IP +" "+PORT);
 
         } catch (IOException e) {
-            textViewStatus.setText(e.getMessage());
+            textViewStatus.setText("Connected "+e.getMessage());
             Log.e("Exception Connection ", " " + e.getMessage());
             e.printStackTrace();
         }
@@ -68,51 +84,79 @@ public class WebSocket extends AppCompatActivity {
 
         try {
             if (clientSocket.isConnected()) {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(
+                        new OutputStreamWriter(clientSocket.getOutputStream()));
 
 //                private InputStream in;
 //                private OutputStream out;
-                PrintWriter out = new PrintWriter(new BufferedWriter(
-                        new OutputStreamWriter(clientSocket.getOutputStream())),
-                        true);
-                out.println(message);
+//                PrintWriter out = new PrintWriter(new BufferedWriter(
+//                        new OutputStreamWriter(clientSocket.getOutputStream())),
+//                        true);
+//                out.println(message);
+//                out.flush();
+
+//                String msg1 = in.readLine()
+//                        + System.getProperty("line.separator");
+//
+//                textViewShowCommand.setText(textViewShowCommand.getText()+" Command Received 1 "+msg1);
+
+                out.write(message + "\n");
+
+//                String msg2 = in.readLine();
+//                textViewShowCommand.setText(textViewShowCommand.getText()+" Command Received 2 "+msg2);
                 out.flush();
-                textViewStatus.setText("*****Message Send ");
+                out.close();
+//                textViewStatus.setText(msg1 + "  " + msg2);
                 Log.e("Server  Connected ", " *****Message Send  ");
             } else {
-                textViewStatus.setText("Socket is not connected "+clientSocket.isConnected());
+                Toast.makeText(this, "Not Connected To Server", Toast.LENGTH_SHORT).show();
+                textViewStatus.setText("Socket is not connected " + clientSocket.isConnected());
+
 //                new Thread(new ClientThread()).start();
             }
         } catch (IOException e) {
             textViewStatus.setText(e.getMessage());
             Log.e("Exception ", "while message Send " + e.getMessage());
+//            connect();
             e.printStackTrace();
         }
     }
 
     public void sendMeesage(View view) {
-        sendMessage("on");
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                sendMessage("off");
-                Toast.makeText(WebSocket.this, "Off Command is Send ", Toast.LENGTH_SHORT).show();
-            }
-        }, 5000);
+        textViewShowCommand.setText(textViewShowCommand.getText()+"\n"+editTextCommand.getText());
+        sendMessage(editTextCommand.getText().toString().trim());
+        editTextCommand.setText("");
+
 //        sendMessage("on");
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                sendMessage("off");
+//                Toast.makeText(WebSocket.this, "Off Command is Send ", Toast.LENGTH_SHORT).show();
+//            }
+//        }, 5000);
+////        sendMessage("on");
     }
 
     public void discountPort(View view) {
 
-            try {
-                if (clientSocket!=null){
+        try {
+            if (clientSocket != null) {
                 clientSocket.shutdownOutput();
                 clientSocket.shutdownInput();
                 clientSocket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
 
         }
+    }
+
+    public void connectTosocket(View view) {
+//        if (!clientSocket.isConnected())
+//        new Thread(new ClientThread()).start();
     }
 
     private class UpdateTask extends AsyncTask<String, String, String> {
@@ -142,6 +186,8 @@ public class WebSocket extends AppCompatActivity {
 
             } catch (Exception e1) {
                 textViewStatus.setText("Failed .... " + e1.getMessage());
+
+                Log.e("Failed "," "+e1.getMessage());
                 e1.printStackTrace();
             }
 
